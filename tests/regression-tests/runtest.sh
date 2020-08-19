afed217ebc522e339e6197b9910c10c4787db7cb
Tom Stellard <tstellar@redhat.com>
2020-08-19 14:39:12 +0000

    Fix regression-tests CI tests

---
+++ tests/regression-tests/runtest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+cmd='/usr/libexec/tests/llvm/run-lit-tests --threads 1'
+if [ `id -u` -eq 0 ]; then
+  # lit tests can't be run as root, so we need to run as a different user
+  user='llvm-regression-tests'
+  if ! id -u $user; then
+    useradd $user
+  fi
+  su $user -c "$cmd"
+  cmd="su $user -c $cmd"
+else
+  exec $cmd
+fi
